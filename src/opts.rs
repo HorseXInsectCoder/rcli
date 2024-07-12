5bd88309aef377d26fd7b4206b09f0e631d0d9af
voyager-1 <cyhzero@gmail.com>
2024-07-13 00:15:34 +0800

    support generic csv to json and yaml output

--- src/opts.rs
+++ src/opts.rs
@@ -1,4 +1,5 @@
-use std::path::Path;
+use core::fmt;
+use std::{path::Path, str::FromStr};
 
 use clap::Parser;
 
@@ -16,6 +17,12 @@ pub enum Subcommand {
     Csv(CsvOpts),
 }
 
+#[derive(Debug, Copy, Clone)]
+pub enum OutputFormat {
+    Json,
+    Yaml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     // 对 input 做合法性检查，可以写自定义函数或者使用 clap 自带的
@@ -23,8 +30,8 @@ pub struct CsvOpts {
     pub input: String,
 
     // default_value 的展开是："output.json".into()，因为 "output.json" 是一个 &str，而 output 要求一个 String
-    #[arg(short, long, default_value = "output.json")]
-    pub output: String,
+    #[arg(short, long)]
+    pub output: Option<String>,
 
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
@@ -33,6 +40,9 @@ pub struct CsvOpts {
     #[arg(long, default_value_t = true)]
     // default_value_t 就是直接传一个字面量，即不会经过 from 转换
     pub header: bool,
+
+    #[arg(long, default_value = "json", value_parser = parse_format)]
+    pub format: OutputFormat,
 }
 
 fn verify_input_file(filename: &str) -> Result<String, String> {
@@ -40,5 +50,46 @@ fn verify_input_file(filename: &str) -> Result<String, String> {
         Ok(filename.into())
     } else {
         Err("File does not exists".into())
+    }
+}
+
+// anyhow::Error 可以转为 String 输出到命令行
+fn parse_format(format: &str) -> Result<OutputFormat, anyhow::Error> {
+    // 有 impl FromStr 后就不再需要使用这段了
+    // match format.to_lowercase().as_str() {
+    //     "json" => Ok(OutputFormat::Json),
+    //     "yaml" => Ok(OutputFormat::Yaml),
+    //     "toml" => Ok(OutputFormat::Toml),
+    //     _ => Err("Invalid format")
+    // }
+
+    // parse 可以把 String 解析成其他的数据类型，前提是这个数据类型实现了 FromStr
+    format.parse()
+}
+
+impl From<OutputFormat> for &'static str {
+    fn from(format: OutputFormat) -> Self {
+        match format {
+            OutputFormat::Json => "json",
+            OutputFormat::Yaml => "yaml",
+        }
+    }
+}
+
+impl FromStr for OutputFormat {
+    type Err = anyhow::Error;
+
+    fn from_str(format: &str) -> Result<Self, Self::Err> {
+        match format.to_lowercase().as_str() {
+            "json" => Ok(OutputFormat::Json),
+            "yaml" => Ok(OutputFormat::Yaml),
+            v => anyhow::bail!("Unsupported format: {}", v),
+        }
+    }
+}
+
+impl fmt::Display for OutputFormat {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
--- src/process.rs
+++ src/process.rs
@@ -1,10 +1,13 @@
 use serde::{Deserialize, Serialize};
 
 use csv::Reader;
+use serde_json::Value;
 
 use std::fs;
 
 use anyhow::Result;
+
+use crate::opts::OutputFormat;
 
 // juventus.csv 的字段结构
 #[derive(Debug, Deserialize, Serialize)]
@@ -25,18 +28,31 @@ struct Player {
     kit: u8, // 球衣号
 }
 
-pub fn process_csv(input: &str, output: &str) -> Result<()> {
-    let mut reader = Reader::from_path(input)?;
+pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
+    let mut reader: Reader<fs::File> = Reader::from_path(input)?;
 
     let mut ret = Vec::with_capacity(128);
 
-    for result in reader.deserialize() {
-        let record: Player = result?;
+    let headers: csv::StringRecord = reader.headers()?.clone();
+
+    for result in reader.records() {
+        let record: csv::StringRecord = result?;
+        // headers.iter() -> 使用 headers 的迭代器
+        // record.iter() -> 使用 record 的迭代器
+        // zip() -> 将两个迭代器合并为一个元组的迭代器 [(header, record), ..]
+        // collect::<Value>() -> 将元组的迭代器转换为 JSON Value
+        // 生成的是 Json Value，但是因为 Json Value 实现了 Deserialize, Serialize，所以最后是 Value
+        let json_value = headers.iter().zip(record.iter()).collect::<Value>();
         // println!("{:?}", record);
-        ret.push(record);
+        ret.push(json_value);
     }
-    let json = serde_json::to_string_pretty(&ret)?;
-    fs::write(output, json)?;
+
+    let content = match format {
+        OutputFormat::Json => serde_json::to_string_pretty(&ret)?,
+        OutputFormat::Yaml => serde_yaml::to_string(&ret)?,
+    };
+
+    fs::write(output, content)?;
 
     Ok(())
 }
